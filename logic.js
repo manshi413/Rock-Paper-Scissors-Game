9d0155654ddad91cdca90b7d05a54fc04131e5c9
Manshi Yadav <manshiyadav413@gmail.com>
2024-06-03 13:51:00 +0530

    updated

---
+++ logic.js
@@ -0,0 +1,13 @@
+mode="light"
+if(mode==="light")
+    {
+        mode="dark";
+        document.querySelector("#lo").classList.add(dark)
+        document.querySelector("#lo").classList.remove(light);
+    }
+else
+{
+    mode="light";
+        document.querySelector("#lo").classList.add(light)
+        document.querySelector("#lo").classList.remove(dark);
+}
