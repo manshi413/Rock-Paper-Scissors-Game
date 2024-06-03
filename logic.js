33e187ed33dfa4a5700efb381183094fc5acb23b
Manshi Yadav <manshiyadav413@gmail.com>
2024-06-03 14:16:57 +0530

    Updated JS

--- logic.js
+++ logic.js
@@ -1,16 +1,17 @@
 let el=document.querySelector("#lo");
+let h=document.querySelector("body");
 mode="light"
 let m=el.addEventListener("click",()=>{
 if(mode==="light")
     {
         mode="dark";
-        document.querySelector("#lo").classList.add("dark")
-        document.querySelector("#lo").classList.remove("light");
+        h.classList.add("dark")
+        h.classList.remove("light");
     }
 else
 {
     mode="light";
-        document.querySelector("#lo").classList.add("light")
-        document.querySelector("#lo").classList.remove("dark");
+        h.classList.add("light")
+        h.classList.remove("dark");
 }
 });
